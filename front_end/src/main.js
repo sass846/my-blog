10eb281264cd7ed097c9d3b3b78890adfb869d38
sass846 <skiran.self@gmail.com>
2024-12-15 09:50:18 +0530

    update components

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -4,6 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql",
+});
+
+const cache = new InMemoryCache();
+
 const app = createApp(App)
 
 app.use(router)
